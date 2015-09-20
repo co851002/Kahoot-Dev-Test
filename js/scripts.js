f507403fb6ff26eb35b14b60bd895312dd26228e
co851002 <co851002@gmail.com>
2015-09-20 20:25:30 +0100

    --all

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function (e) {
+                    $('#imageContainer')
+                        .attr('src', e.target.result)
+                        .width(150)
+                        .height(200);
+                };
+
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+
+$( document ).ready(function() {
+/*$("input").focusin(function() {
+    $("#instructionMessage").hide();
+    }).focusout(function () {
+    $("#instructionMessage").show();
+});*/
+
+$("input.required").keyup(function(){
+    if($(this).val()) {
+        $("#instructionMessage").hide("slow");
+    } else {
+        $("#instructionMessage").show("slow");
+    }
+        
+});
+
+$(".closeMessage").click(function(){
+    $("#instructionMessage").hide();
+});
+
+});
